18a176d48c0f256b169d6c38be23c43ca53adba9
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-14 17:30:20 +0100

    finisco più tardi

--- query-videogiochi.sql
+++ query-videogiochi.sql
@@ -146,18 +146,55 @@
 --QUERY CON JOIN
 
 --1- Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
+--SELECT DISTINCT player_id ,name,lastname,city
+--FROM players
+--INNER JOIN reviews
+--ON players.id = reviews.player_id
 
 
 
 
 
 --2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
+--SELECT videogame_id
+--FROM tournaments
+--INNER JOIN tournament_videogame
+--ON tournaments.id = tournament_id
+--WHERE year = '2016'
+--GROUP BY videogame_id
+
+
+
 
 --3- Mostrare le categorie di ogni videogioco (1718)
+--SELECT *
+--FROM category_videogame
+--INNER JOIN categories
+--ON categories.id = category_id
+
+
+
 
 --4- Selezionare i dati di tutte le software house che hanno rilasciato almeno un gioco dopo il 2020, mostrandoli una sola volta (6)
+--SELECT count(videogames.id) 
+--FROM software_houses
+--INNER JOIN videogames
+--ON videogames.software_house_id = software_houses.id
+--WHERE release_date  >='2020-01-01'
+--GROUP BY software_houses.id
+
+
+
 
 --5- Selezionare i premi ricevuti da ogni software house per i videogiochi che ha prodotto (55)
+--SELECT *
+--FROM award_videogame
+--INNER JOIN awards
+--ON awards.id = award_videogame.award_id
+--INNER JOIN videogames
+--ON videogames.id = award_videogame.videogame_id
+--INNER JOIN software_houses
+--ON videogames.software_house_id = software_houses.id
 
 --6- Selezionare categorie e classificazioni PEGI dei videogiochi che hanno ricevuto recensioni da 4 e 5 stelle, mostrandole una sola volta (3363)
 
