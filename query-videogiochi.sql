508e462033954671e30aaf460b5aa4e0612decae
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-14 16:46:27 +0100

    push di  intramezzo

--- query-videogiochi.sql
+++ query-videogiochi.sql
@@ -97,20 +97,59 @@
 --QUERY CON GROUPBY
 
 --1- Contare quante software house ci sono per ogni paese (3)
+--SELECT count(id)
+--FROM software_houses
+--GROUP BY country
+
+
+
 
 --2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
+--SELECT count(videogame_id)
+--FROM reviews
+--GROUP BY videogame_id
+
+
 
 --3- Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
+--SELECT count(videogame_id)
+--FROM pegi_label_videogame
+--GROUP BY pegi_label_id
+
+
+
+
 
 --4- Mostrare il numero di videogiochi rilasciati ogni anno (11)
+--SELECT count(id)
+--FROM videogames
+--GROUP BY YEAR(release_date)
+
+
 
 --5- Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
+--SELECT count(videogame_id)
+--FROM device_videogame
+--GROUP BY device_id
+
+
+
 
 --6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
+--SELECT videogame_id, AVG(rating)
+--FROM reviews
+--GROUP BY videogame_id
+--ORDER BY AVG(rating) DESC
+
+
 
 --QUERY CON JOIN
 
 --1- Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
+
+
+
+
 
 --2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
 
